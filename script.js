041397c2ffa155bd0e016a0b158a620a92036bf7
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-01-01 16:42:59 -0500

    final changes, finish

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+function openNav() {
+    var links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
     } else {
-      x.style.display = "block";
+      links.style.display = "block";
     }
   }
