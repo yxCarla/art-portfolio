8ce060bd4e09d63773203e65fc438d20ddb92361
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-03-19 22:02:08 -0400

    fixed cards

--- script.js
+++ script.js
@@ -68,154 +68,237 @@ function changeColumns() {
   var columnSize = document.getElementById('columnSizing');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing2');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing3');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing4');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing5');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing6');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing7');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing8');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }  
   var columnSize = document.getElementById('columnSizing9');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing10');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing11');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing12');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing13');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing14');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing15');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing16');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing17');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing18');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing19');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing20');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing21');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing22');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing23');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing24');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing25');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing26');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing27');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing28');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing29');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing30');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   }
   var columnSize = document.getElementById('columnSizing31');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
   } 
   var columnSize = document.getElementById('columnSizing32');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
   var columnSize = document.getElementById('columnSizing34');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   }
   var columnSize = document.getElementById('columnSizing35');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
   var columnSize = document.getElementById('columnSizing36');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
   var columnSize = document.getElementById('columnSizing37');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
   var columnSize = document.getElementById('columnSizing38');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
   var columnSize = document.getElementById('columnSizing39');
   if(myRange.value == '2') {
     columnSize.classList.add('col-6');
+    columnSize.classList.remove('col-3');
+    columnSize.classList.remove('col-4');
+
   } 
 
   
